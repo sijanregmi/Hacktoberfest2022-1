6ca700367cd57c072b4aab6e35fec72c06c13618
Samiksha sandip sankar <samikshasankar2000@gmail.com>
2022-10-03 18:28:17 +0530

    Create NumberOfDays.java

---
+++ Scripts/NumberOfDays.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+public class NumberOfDays {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+       int n =sc.nextInt();
+  switch (n){
+  case 1:
+    System.out.println("31");
+    break;
+  case 2:
+    System.out.println("28");
+    break;
+  case 3:
+    System.out.println("31");
+    break;
+  case 4:
+    System.out.println("30");
+    break;
+  case 5:
+    System.out.println("31");
+    break;
+  case 6:
+    System.out.println("30");
+    break;
+  case 7:
+    System.out.println("31");
+    break;
+  case 8:
+    System.out.println("31");
+    break;
+  case 9:
+    System.out.println("30");
+    break; 
+  case 10:
+    System.out.println("31");
+    break;
+  case 11:
+    System.out.println("30");
+    break;
+  case 12:
+    System.out.println("31");
+    break;
+      }
+    }
+}
